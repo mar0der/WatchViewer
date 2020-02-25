00691350a3d0b44729ac5b427cca2249869a86f0
Petar Petkov <petar.b.petkov@gmail.com>
2020-02-25 08:12:27 +0400

    Initial Commit

---
+++ WatchViewer WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchViewer WatchKit Extension
+//
+//  Created by Petar Petkov on 25/02/2020.
+//  Copyright © 2020 Petar Petkov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
